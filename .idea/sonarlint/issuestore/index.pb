ca85af047e3a726139b0fef735fd5090c1d508c3
danieltikamori <tikamori.itiro@gmail.com>
2024-04-14 20:37:18 +0900

    Update code

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+Y
+)src/cc/tkmr/infrastructure/Principal.java,3/f/3fcdc21c93e7c796844bc4eeea568ebb3bc56489
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8/8/88a9255124c95bdc913197c120a8d560edc59c8e
+Å
+Q.idea/sonarlint/securityhotspotstore/3/f/3fcdc21c93e7c796844bc4eeea568ebb3bc56489,5/c/5c5fd131afef25bd1acbf807c5a3228031d12a58
--- src/cc/tkmr/infrastructure/Principal.java
+++ src/cc/tkmr/infrastructure/Principal.java
@@ -2,37 +2,25 @@ package cc.tkmr.infrastructure;
 
 import java.util.Scanner;
 
-interface NotificacaoEmail {
-  void enviarEmail(String mensagem);
-}
-
-interface NotificacaoSMS {
-  void enviarSMS(String mensagem);
-}
-
-class NotificacaoEmailImpl implements NotificacaoEmail {
-  @Override
-  public void enviarEmail(String mensagem) {
-    System.out.println("Email enviado: %s".formatted(mensagem));
-  }
-}
-
-class NotificacaoSMSImpl implements NotificacaoSMS {
-  @Override
-  public void enviarSMS(String mensagem) {
-    System.out.println("SMS enviado: %s".formatted(mensagem));
-  }
-}
-
 public class Principal {
   public static void main(String[] args) {
     try(Scanner scanner = new Scanner(System.in)) {
       String[] entradas = scanner.nextLine().split(", ");
-  
+
       String tipoNotificacao = entradas[0];
       String mensagem = entradas[1];
-  
+
       // TODO: Criar objetos específicos com base no tipo escolhido.
+      // TODO: Criar objetos específicos com base no tipo escolhido.
+      if (tipoNotificacao.equalsIgnoreCase("email")) {
+        NotificacaoEmail notificacaoEmail = new NotificacaoEmailImpl();
+        notificacaoEmail.enviarEmail(mensagem);
+      } else if (tipoNotificacao.equalsIgnoreCase("sms")) {
+        NotificacaoSMS notificacaoSMS = new NotificacaoSMSImpl();
+        notificacaoSMS.enviarSMS(mensagem);
+      } else {
+        System.out.println("Tipo de notificacao invalido.");
+      }
     }
   }
 }
